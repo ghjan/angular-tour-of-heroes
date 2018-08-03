445fc338f7f642f09120dea7a3261715bd93ffb2
jian.zhang <cajan2@163.com>
2018-08-03 13:14:42 +0800

    save

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,29 +1,95 @@
 import {Injectable} from '@angular/core';
 import {Hero} from './hero';
-import {HEROES} from './mock-heroes';
+// import {HEROES} from './mock-heroes';
 import {Observable, of} from 'rxjs';
 import {MessageService} from './message.service';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {catchError, map, tap} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class HeroService {
+  static httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+  private heroesUrl = 'api/heroes';  // URL to web api
+
   // Angular 将会在创建 HeroService 时把 MessageService 的单例 注入到这个属性中。
-  constructor(private messageService: MessageService) {
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) {
   }
 
   // getHeroes(): Hero[] {   return HEROES; }
 
+  // getHeroes(): Observable<Hero[]> {
+  //   // TODO: send the message _after_ fetching the heroes
+  //   this
+  //     .log('HeroService: fetched heroes');
+  //   return of(HEROES);
+  // }
+
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this
-      .messageService
-      .add('HeroService: fetched heroes');
-    return of(HEROES);
+    // HttpClient.get 默认情况下把响应体当做无类型的 JSON 对象进行返回。
+    // 如果指定了可选的模板类型 <Hero[]>，就会给返回你一个类型化的对象。
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(heroes => this.log('fetched heroes')), // 窥探 Observable
+      catchError(this.handleError('getHeroes', []))
+    );
   }
 
-  getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    // 反引号 ( ` ) 用于定义 JavaScript 的 模板字符串字面量，以便嵌入 id
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
+
+  // getHero(id: number): Observable<Hero> {
+  //   // TODO: send the message _after_ fetching the hero
+  //   // 反引号 ( ` ) 用于定义 JavaScript 的 模板字符串字面量，以便嵌入 id
+  //   this.log(`HeroService: fetched hero id=${id}`);
+  //   return of(HEROES.find(hero => hero.id === id));
+  // }
+
+  /** GET hero by id. Will 404 if id not found */
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, HeroService.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  // search(term: string): Observable<Hero[]> {
+  //   return this.http
+  //     .get(`api/heroes/?name=${term}`)
+  //     .map(response => response.json().data as Hero[]);
+  // }
 }
